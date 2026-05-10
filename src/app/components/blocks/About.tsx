b9e5be757ab069f3683f989618eefe8b0147e6bc
Gxbriel99 <gabriel.perini99@gmail.com>
2026-05-10 15:08:59 +0200

    Inizializzato block about

---
+++ src/app/components/blocks/About.tsx
@@ -0,0 +1,8 @@
+
+export default function Aboute() {
+  return (
+    <section className="w-full">
+      <p>About</p>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Faq from "./components/blocks/Faq";
 import Header from "./components/blocks/Header";
 import Service from "./components/blocks/Service";
 import Testimonials from "./components/blocks/Testimonials";
+import About from "./components/blocks/About";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <About />
       <Service />
       <Faq />
       {/* <Testimonials /> */}
