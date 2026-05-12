0f8192e88295bf2b2a5441ff0d5ef61101268a7a
Gxbriel99 <gabriel.perini99@gmail.com>
2026-05-12 22:49:56 +0200

    Implemenato sfondo about

--- src/app/components/blocks/About.tsx
+++ src/app/components/blocks/About.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
 
-export default function Aboute() {
+export default function About() {
   return (
-    <section className="w-full">
-      <p>About</p>
+    <section
+      id="chi-siamo"
+      className="relative z-[3] h-screen w-full overflow-visible bg-white"
+    >
+      <div
+        className="pointer-events-none absolute inset-x-0 -top-26 -bottom-16"
+        style={{
+          backgroundImage: 'url("/about_bg.webp")',
+          backgroundPosition: "center top",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      />
     </section>
   );
 }
